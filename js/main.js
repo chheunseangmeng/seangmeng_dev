4ba4d33cec871bf5ef08116bbdd72cd0d81e79e8
chheunseangmeng <seangmeng.chheun@student.passerellesnumeriques.org>
2025-07-03 20:57:15 +0700

    added img vc

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const tl = gsap.timeline({ paused: true });
 
   tl.to(navLinks, {
-    x: 0,  // Slide in from right
+    x: 0, // Slide in from right
     opacity: 1,
     visibility: "visible",
     duration: 0.5,
@@ -45,35 +45,58 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 const items = document.querySelectorAll(".education-timeline-item");
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-}, {
-  threshold: 0.1
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
 
-items.forEach(item => {
+items.forEach((item) => {
   observer.observe(item);
 });
 
-
-
 // Initialize ScrollReveal
-ScrollReveal().reveal('.scroll-reveal', {
+ScrollReveal().reveal(".scroll-reveal", {
   delay: 150,
   duration: 1000,
-  distance: '50px',
-  origin: 'bottom',
-  easing: 'ease-in-out',
-  reset: false,     
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  reset: false,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const progressBars = document.querySelectorAll(".progress-fill");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animated");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.5,
+  });
+
+  progressBars.forEach((bar) => observer.observe(bar));
 });
 
 
-
-// my education
+function showNotDeployedAlert() {
+    Swal.fire({
+      icon: 'info',
+      title: 'Not Deployed',
+      text: 'This project website has not deploy yet.\nGo to Git Hub',
+      confirmButtonText: 'OKAY'
+    });
+  }
